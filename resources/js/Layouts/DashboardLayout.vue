a793b9468ba7d103946442175bb768e24103aef2
MTPaulette <mayoguepaulette@gmail.com>
2023-03-30 12:51:33 +0200

    codons nous meme notre sidebar

--- resources/js/Layouts/DashboardLayout.vue
+++ resources/js/Layouts/DashboardLayout.vue
@@ -1,37 +1,7 @@
 <template>
-  <div id="main-body" class="leading-normal tracking-normal">
-    <div class="flex flex-wrap">
-      <Sidebar />
-      <div id="main-content" class="w-full bg-gray-100 pl-0 lg:pl-64 min-h-screen" :class="sideBarOpen ? 'overlay' : ''">
-        <Navbar />
+  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam libero vitae,
+  architecto doloremque voluptatibus maiores velit veritatis optio error possimus 
+  unde asperiores molestiae quia officiis vero, nisi iusto distinctio. Tempore?
 
-        <div class="p-6 bg-gray-100 mb-20">
-          <slot />
-        </div>
-
-        <MyFooter />
-      </div>
-    </div>
-  </div>
+  <slot />
 </template>
-
-<script>
-import { mapState } from 'vuex'
-
-
-import Sidebar from '@/Components/Dashboard/Sidebar.vue'
-import Navbar from '@/Components/Dashboard/Navbar.vue'
-import MyFooter from '@/Components/Dashboard/Footer.vue'
-
-export default {
-  name: 'Dashboard',
-  Components: {
-    Sidebar,
-    Navbar,
-    MyFooter,
-  },
-  computed: {
-    ...mapState(['sideBarOpen']),
-  },
-}
-</script>
