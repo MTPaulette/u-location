5e4c010bdd721fecad61eabf74586109daf0efca
MTPaulette <mayoguepaulette@gmail.com>
2023-08-27 14:13:34 +0200

    modification du lien de la sidebar pour afficher slmt le profile pour un utilisateur non admin

--- resources/js/Components/Navbar/tools/loggedUser.vue
+++ resources/js/Components/Navbar/tools/loggedUser.vue
@@ -1,9 +1,15 @@
 <template>
-  <button id="user-menu-button" type="button" class="flex mx-2 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
+  <!-- <button id="user-menu-button" type="button" class="flex mx-2 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
     <div class="flex-shrink-0">
       <img class="w-8 h-8 rounded-full" src="./../../../../images/img.PNG" alt="user photo" />
     </div>
+  </button> -->
+  <button id="user-menu-button" type="button" class="flex mx-2 p-1 text-sm bg-gray-200 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
+    <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 16 16">
+      <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z" />
+    </svg>
   </button>
+
   <div id="user-dropdown" class="z-50 hidden my-4 text-base list-none bg-gray-50 divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600">
     <div v-if="user" class="px-4 py-3">
       <span class="block text-sm text-gray-900 dark:text-white">{{ user.role_id }}</span>
--- resources/js/Components/Sidebar/Sidebar.vue
+++ resources/js/Components/Sidebar/Sidebar.vue
@@ -88,23 +88,15 @@
             </li> -->
           </ul>
         </li>
+        
         <li v-else>
-          <button type="button" class="sidebar-item" aria-controls="dropdown-user" data-collapse-toggle="dropdown-user">
+          <Link
+            href="/profile" class="sidebar-item"
+            :class="{'active-sidebar-link': $page.url.startsWith('/profile')}"
+          >
             <svg aria-hidden="true" class="sidebar-icon" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd" /></svg>
-            <span class="flex-1 ml-3 text-left whitespace-nowrap" sidebar-toggle-item>Users</span>
-            <svg sidebar-toggle-item class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" /></svg>
-          </button>
-          <ul id="dropdown-user" class="hidden py-2 space-y-2">
-            <li>
-              <Link
-                href="/profile" 
-                :class="{'active-sidebar-link': $page.url.startsWith('/profile')}"
-                class="sidebar-item-dropdown"
-              >
-                Profile
-              </Link>
-            </li>
-          </ul>
+            <span class="ml-3">Profile</span>
+          </Link>
         </li>
         
         <li>
